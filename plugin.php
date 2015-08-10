3b2a717e82d28de4e02dec90dbe9d8251f83f6d9
Awais <owasii@gmail.com>
2015-08-11 01:17:57 +0200

    initialized

---
+++ plugin.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name:       PackR
+ * Plugin URI:        http://gitlab.vislog.net:9090/Awais/PackR
+ * Description:       Package Registration for vislog.
+ * Version:           1.0.0
+ * Author:            Awais
+ * Author URI:        http://fotaxis.com/
+ * License:           MIT
+ */
+
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/getherbert/framework/bootstrap/autoload.php';
